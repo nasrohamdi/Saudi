50e1ce6bacead56dcdbed2ce2b5a68ebf55241ff
nasrohamdi <107858530+nasrohamdi@users.noreply.github.com>
2023-02-04 17:59:32 +0100

    Create Prfooter.jsx

---
+++ src/pages/Prfooter.jsx
@@ -0,0 +1,55 @@
+
+import React from 'react'
+
+
+const Write = () => {
+
+    return (
+        <div class="container" style={{width:'500px'}}>
+            <div class="row  ">
+
+                <div class="col-sm-6 disp3">
+                    <div class="row border-0" style={{ backgroundColor: '#ECEAE8' }}>
+                        <div class="col-5 col-sm-6">
+                            <div class="card-column  ">
+                                <div className='row disp3'>
+                                
+                                    <div class="col my-4 sm-2 disp3">
+                                    <button class="btn btn-success " style={{width:'80px',textAlign:'right'}}>إشترك</button>
+                                    <input class="form-control   text-small " type="search" placeholder="البريد الالكتروني" aria-label="Search" style={{ width: '110px', height: '30px', alignItems:'end' }} />
+
+                                    </div>
+                                    
+
+                                </div>
+                            
+                            
+                            </div>
+                        </div>
+                        <div class="col-4 col-sm-6">
+                            
+                            <div className="d-flex">
+                                <div className="card-columns d-flex">
+                                    <div class="card p-2 mx-auto  border-0" id="c1" style={{ width: '34rem', height: '120px',backgroundColor:'#ECEAE8'}} >
+                                        <div class="card-body">
+
+                                            <h5 class="card-title text-gras">اﻧﻀﻢ اﻟﻴﻨﺎ</h5>
+                                            <p class="card-text text-small">ﻫﺬا اﻟﻨﺺ ﻫﻮ ﻣﺜﺎل ﻟﻨﺺ ﻳﻤﻜﻦ أن ﻳﺴﺘﺒﺪل ﻓﻲ ﻧﻔﺲ اﻟﻤﺴﺎﺣﺔ ﺣﻴﺚ ﻳﻤﻜﻨﻚ أن ﺗﻮﻟﺪ ﻣﺜﻞ ﻫﺬا اﻟﻨﺺ أو اﻟﻌﺪﻳﺪ ﻣﻦ اﺷﺘﺮاك ، اﻟﻨﺺ ﻣﻦ ﻣﻮﻟﺪ اﻟﻨﺺ اﻟﻌﺮﺑﻰ </p>
+                                            
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div></div>
+
+
+    )
+
+
+}
+export default Write;
