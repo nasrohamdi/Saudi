0c1ff3e4e4d7b1af177b20d519476330e0bf5abf
nasrohamdi <107858530+nasrohamdi@users.noreply.github.com>
2023-02-04 17:59:26 +0100

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,288 @@
+import React from 'react'
+import im2 from '../images/anno2.png';
+import im3 from '../images/anno3.png';
+import im5 from '../images/evt1.png';
+import im6 from '../images/evt2.png';
+import im7 from '../images/evt3.png';
+import im8 from '../images/evt4.png';
+import im9 from '../images/share.png';
+import im10 from '../images/horl.png';
+import im11 from '../images/location.png';
+import im12 from '../images/aime.png';
+import im13 from '../images/comm.png';
+import im14 from '../images/vue.png';
+const Home=()=>{
+
+return(
+<div class="container my-5">
+        <div class="row">
+
+          <div class="col">
+            <h2 class="text-gras">أخر الأخبار</h2>
+            <div class="col">
+
+              <div class="card" style={{ border: '0px', width: '16rem' }}>
+                <div class="col" >
+                  <img src={im2} class="card-img-top my-2 mx-1" alt="..." style={{ width: '240px' }} />
+
+                </div>
+                <div class="card-body">
+                  <h2><p class="text-gras card-text ">بجوائز قيمتها 200 مليون ريال الرياض تستعد لتنظيم دورات للالعاب السعودية </p></h2>
+                </div>
+                <p class="card-text"><small class="text-muted disp">
+                  <h6 className='text-small' >74582</h6> 
+                       <img class="im" src={im14} alt=""></img>
+                       
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small' >74582</h6>
+                        <img  class="imp1" src={im13} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small">
+                          <h6 className='text-small'>1254</h6>
+                          <img  class="imp1" src={im12} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></p>
+              </div>
+
+            </div>
+            <div class="col">
+
+              <div class="card" style={{ border: '0px', width: '16rem' }}>
+                <img src={im3} class="card-img-top my-2 mx-1" alt="..." style={{ width: '240px' }} />
+                <div class="card-body">
+                  <h2><p class="card-text text-gras">بجوائز قيمتها 200 مليون ريال الرياض تستعد لتنظيم دورات للالعاب السعودية </p></h2>
+                </div>
+                <p class="card-text"><small class="text-muted disp">
+                  <h6 className='text-small' >74582</h6> 
+                       <img class="im" src={im14} alt=""></img>
+                       
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small' >74582</h6>
+                        <img  class="imp1" src={im13} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small">
+                          <h6 className='text-small'>1254</h6>
+                          <img  class="imp1" src={im12} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></p>
+              </div>
+
+
+            </div>
+            <div class="col">
+
+              <div class="col">
+
+                <div class="card" style={{ border: '0px', width: '16rem' }}>
+                  <img src={im3} class="card-img-top my-2 mx-1" alt="..." style={{ width: '240px' }} />
+                  <div class="card-body">
+                    <h2><p class="card-text text-gras">بجوائز قيمتها 200 مليون ريال الرياض تستعد لتنظيم دورات للالعاب السعودية </p></h2>
+                  </div>
+                  <p class="card-text"><small class="text-muted disp">
+                  <h6 className='text-small' >74582</h6> 
+                       <img class="im" src={im14} alt=""></img>
+                       
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small' >74582</h6>
+                        <img  class="imp1" src={im13} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small">
+                          <h6 className='text-small'>1254</h6>
+                          <img  class="imp1" src={im12} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></p>
+                </div>
+
+
+              </div>
+
+
+            </div>
+
+
+
+
+
+          </div>
+          <div class="col mx-2" >
+            <h2 class="text-gras">الفعاليات</h2>
+            <div class="col"  >
+
+              <div class="card mb-3" style={{ border: '0px', backgroundColor: '#ECEAE8', width: '27rem' }}>
+
+                <div class="row g-0" >
+                  <div class="col-md-8">
+
+
+                    <div class="card-body">
+                      <h5 class="card-title text-gras">المعرض السعودي للفعاليات</h5>
+                      <p class="card-text text-small">يقام معرض الرياض الدولي للكتاب بشكل سنوي في مدينة الرياض حيثٌ يعد منصة للشركات والمؤسسات والأفراد العاملين والمهتمين بقطاعات ...</p>
+                      <p class="card-text"><small class="text-muted"><small class="text-muted disp">
+                       <img class="imp1 text-small1" src={im9} alt=""></img> 
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small2' style={{  width: '40px'}}>ص 12:00  </h6>
+                        <img  class="imp1" src={im10} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small1">
+                          <h6 className='text-small2'style={{  width: '140px'}}>  مركز الرياض الدولي للمؤتمرات والمعارض</h6>
+                          <img  class="imp1" src={im11} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></small></p>
+                    </div>
+
+                  </div>
+                  <div class="col-md-4" >
+
+                    <img src={im5} class="card-img-top my-2 mx-1" alt="..." style={{ width: '125px', height: '130px' }} />
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div class="col"  >
+
+              <div class="card mb-3" style={{ border: '0px', backgroundColor: '#ECEAE8', width: '27rem' }} >
+
+                <div class="row g-0" >
+                  <div class="col-md-8">
+
+
+                    <div class="card-body">
+                      <h5 class="card-title text-gras">المعرض السعودي للفعاليات</h5>
+                      <p class="card-text text-small">يقام معرض الرياض الدولي للكتاب بشكل سنوي في مدينة الرياض حيثٌ يعد منصة للشركات والمؤسسات والأفراد العاملين والمهتمين بقطاعات ...</p>
+                      <p class="card-text"><small class="text-muted disp">
+                       <img class="imp1 text-small1" src={im9} alt=""></img> 
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small2'  style={{  width: '40px'}}>ص 12:00  </h6>
+                        <img  class="imp1" src={im10} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small1">
+                          <h6 className='text-small2'style={{  width: '140px'}}>  مركز الرياض الدولي للمؤتمرات والمعارض</h6>
+                          <img  class="imp1" src={im11} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></p>
+                    </div>
+
+                  </div>
+                  <div class="col-md-4" >
+
+                    <img src={im6} class="card-img-top my-2 mx-1" alt="..." style={{ width: '125px', height: '130px' }} />
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div class="col">
+
+              <div class="card mb-3" style={{ border: '0px', backgroundColor: '#ECEAE8', width: '27rem' }}>
+                <div class="row g-0" >
+                  <div class="col-md-8">
+
+
+                    <div class="card-body">
+                      <h5 class="card-title text-gras">المعرض السعودي للفعاليات</h5>
+                      <p class="card-text text-small">يقام معرض الرياض الدولي للكتاب بشكل سنوي في مدينة الرياض حيثٌ يعد منصة للشركات والمؤسسات والأفراد العاملين والمهتمين بقطاعات ...</p>
+                      <p class="card-text"><small class="text-muted"><small class="text-muted disp">
+                       <img class="imp1 text-small1" src={im9} alt=""></img> 
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small2'  style={{  width: '40px'}}>ص 12:00  </h6>
+                        <img  class="imp1" src={im10} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small1">
+                          <h6 className='text-small2'style={{  width: '140px'}}>  مركز الرياض الدولي للمؤتمرات والمعارض</h6>
+                          <img  class="imp1" src={im11} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></small></p>
+                    </div>
+
+                  </div>
+                  <div class="col-md-4" >
+                    <img src={im7} class="card-img-top my-2 mx-1" alt="..." style={{ width: '125px', height: '130px' }} />
+
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div class="col">
+
+              <div class="card mb-3" style={{ border: '0px', backgroundColor: '#ECEAE8', width: '27rem' }}>
+                <div class="row g-0" >
+                  <div class="col-md-8">
+
+
+                    <div class="card-body">
+                      <h5 class="card-title text-gras">المعرض السعودي للفعاليات</h5>
+                      <p class="card-text text-small">يقام معرض الرياض الدولي للكتاب بشكل سنوي في مدينة الرياض حيثٌ يعد منصة للشركات والمؤسسات والأفراد العاملين والمهتمين بقطاعات ...</p>
+                      <p class="card-text"><small class="text-muted"><small class="text-muted disp">
+                       <img class="imp text-small1" src={im9} alt=""></img> 
+                       <h5 class="disp text-small1">
+                        <h6 className='text-small2' style={{  width: '40px'}}>ص 12:00  </h6>
+                        <img  class="imp1" src={im10} alt=""/>
+                        </h5>
+                       <h5 class="disp text-small1">
+                          <h6 className='text-small2'style={{  width: '140px'}}>  مركز الرياض الدولي للمؤتمرات والمعارض</h6>
+                          <img  class="imp1" src={im11} alt=""/>
+
+
+                       </h5>
+
+
+
+                      </small></small></p>
+                    </div>
+
+                  </div>
+                  <div class="col-md-4" >
+
+                    <img src={im8} class="card-img-top my-2 mx-1" alt="..." style={{ width: '125px', height: '130px' }} />
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+)
+}
+export default Home;
